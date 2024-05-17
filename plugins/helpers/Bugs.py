e48e819adb5edbc67f85f9b22392f81cf8b021db
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-05-17 19:54:22 +0530

    Update Bugs.py

--- plugins/helpers/Bugs.py
+++ plugins/helpers/Bugs.py
@@ -5,14 +5,14 @@ from info import LOG_CHANNEL
 @feed.on_message(filters.command("feedback"))
 async def feedda(client, message):
   fa = message.text.split(" ", 1)[1]
-  await message.reply_text(f"Hi {message.from_user.mention},\n Thanks for Feed Back 😊")
+  await message.reply_text(f"Hi {message.from_user.mention},\nThanks for Feed Back 😊")
 
   await client.send_message(LOG_CHANNEL, text=f"#Feedbackfromvazha\n\nfeeed back : <code>{fa}</code> user : {message.from_user.mention}") # 😁
 
 @feed.on_message(filters.command("bug"))
 async def bug(client, message):
   bug = message.text.split(" ", 1)[1]
-  await message.reply_text(f"Hi {message.from_user.mention},\n Successfully Reported bugs to my devploers ")
+  await message.reply_text(f"Hi {message.from_user.mention},\nSuccessfully Reported bugs to my devploers ")
 
   await client.send_message(LOG_CHANNEL, text=f"#error \n\nfrom {message.from_user.mention}\n error mes: <code>{bug}</code>")
   
