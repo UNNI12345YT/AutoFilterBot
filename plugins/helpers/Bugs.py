7449f427d6239fb8bbfd416ef50d27c235cd424f
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-17 17:30:43 +0530

    Update Bugs.py

--- plugins/helpers/Bugs.py
+++ plugins/helpers/Bugs.py
@@ -3,17 +3,6 @@ from pyrogram.types import *
 from info import LOG_CHANNEL
 
 @feed.on_message(filters.command("feedback"))
-async def feeeds(client, message):
-  await message.reply_text("/fp - To Send Your Feedback By Publivally\n /fa - to send your feedback by Anonymous")
-
-@feed.on_message(filters.command("fp"))
-async def baka(client, message):
-  fp = message.text.split(" ", 1)[1]
-  await message.reply_text(f"Hi {message.from_user.mention},\n Thanks for Feed Back 😊")
-
-  await client.send_message(LOG_CHANNEL, text=f"#Feedbackfromvazha\n\nfeeed back from {message.from_user.mention}\n text : <code>{fp}</code>")
-
-@feed.on_message(filters.command("fa"))
 async def feedda(client, message):
   fa = message.text.split(" ", 1)[1]
   await message.reply_text(f"Hi {message.from_user.mention},\n Thanks for Feed Back 😊")
