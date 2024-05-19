40a7184f73040f53c87fc3f2b28cfff7e89c93bf
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-19 15:15:51 +0530

    Added

--- plugins/helpers/Bugs.py
+++ plugins/helpers/Bugs.py
@@ -1,15 +1,15 @@
-from pyrogram import Client as feed, filters
+from pyrogram import Client, filters
 from pyrogram.types import *
 from info import LOG_CHANNEL
 
-@feed.on_message(filters.command("feedback"))
+@Client.on_message(filters.command("feedback"))
 async def feedda(client, message):
   fa = message.text.split(" ", 1)[1]
   await message.reply_text(f"Hi {message.from_user.mention},\nThanks for Feed Back 😊")
 
-  await client.send_message(LOG_CHANNEL, text=f"#Feedbackfromvazha\n\nfeeed back : <code>{fa}</code> user : {message.from_user.mention}") # 😁
+  await client.send_message(LOG_CHANNEL, text=f"#Feedbackfromvazha\n\nfeeed back : <code>{fa}</code> user : {message.from_user.mention}") 
 
-@feed.on_message(filters.command("bug"))
+@Client.on_message(filters.command("bug"))
 async def bug(client, message):
   bug = message.text.split(" ", 1)[1]
   await message.reply_text(f"Hi {message.from_user.mention},\nSuccessfully Reported Bugs To My Developer ")
