6eda581f94e8e190fbc3dc1207607e5fd407bf97
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-25 13:02:40 +0530

    Done

--- plugins/helpers/echo.py
+++ plugins/helpers/echo.py
@@ -3,12 +3,12 @@ from info import *
 from pyrogram.types import *
 
 @Client.on_message(filters.command("echo"))
-async def echo(_, message):
-   user = await client.get_chat_member(message.chat.id , message.from_user.id)
-     if user.status not in [enums.ChatMemberStatus.OWNER , enums.ChatMemberStatus.ADMINISTRATOR]:
-         raise PermissionError("You are not allowed to use this command")
-   reply = message.reply_to_message
-   chat_id = message.chat.id
-   if len(message.text.split()) > 1 and reply:
-       await reply.reply_text(message.text.split(None, 1)[1]) 
-   await message.delete()     
+async def echo(client, message):
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
+        raise PermissionError("You are not allowed to use this command")
+    reply = message.reply_to_message
+    chat_id = message.chat.id
+    if reply and len(message.text.split()) > 1:
+        await reply.reply_text(message.text.split(None, 1)[1])
+    await message.delete()
