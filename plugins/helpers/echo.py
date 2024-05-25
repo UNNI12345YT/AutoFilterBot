a0bc9a0396e8a7a0d3e9c734f20167fb3414b289
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-25 12:56:20 +0530

    Added

--- plugins/helpers/echo.py
+++ plugins/helpers/echo.py
@@ -1,8 +1,12 @@
 from pyrogram import Client, filters, enums
-from info import ADMINS
+from info import *
+from pyrogram.types import *
 
-@Client.on_message(filters.command("echo") & filters.user(ADMINS))
+@Client.on_message(filters.command("echo"))
 async def echo(_, message):
+   user = await client.get_chat_member(message.chat.id , message.from_user.id)
+     if user.status not in [enums.ChatMemberStatus.OWNER , enums.ChatMemberStatus.ADMINISTRATOR]:
+         raise PermissionError("You are not allowed to use this command")
    reply = message.reply_to_message
    chat_id = message.chat.id
    if len(message.text.split()) > 1 and reply:
