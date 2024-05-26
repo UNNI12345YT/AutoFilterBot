e197cc9422a9d8f35da73fe9c23387d86c932a7e
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-26 17:39:44 +0530

    Update eval.py

--- plugins/helpers/eval.py
+++ plugins/helpers/eval.py
@@ -1,61 +1,25 @@
-from pyrogram import *
-import sys
-import io
-import time
-import os
+from pyrogram import Client, filters
+from pyrogram.errors import MessageTooLong
+import sys, os
+import re
 import traceback
-import pyrogram 
-import requests as req
-from info import *
+from io import StringIO
+from info import ADMINS
 
-
-async def aexec(code, bot, message, my, m, r, ru):
-    exec(
-        "async def __aexec(bot, message, my, m, r, ru): "
-        + "".join(f"\n {l_}" for l_ in code.split("\n"))
-    )
-    return await locals()["__aexec"](bot, message, my, m, r, ru)
-
-
- 
-def p(*args, **kwargs):
-    print(*args, **kwargs)
-	
-
-@Client.on_message(filters.command('eval') & filters.user(ADMINS))
-async def evaluate(bot, message):
-    
-    status_message = await message.reply("`Running Code...`")
+@Client.on_message(filters.command("eval") & filters.user(ADMINS))
+async def executor(client, message):
     try:
-        cmd = message.text.split(maxsplit=1)[1]
-    except IndexError:
-        await status_message.delete()
-        return
-    start_time = time.time()
-
-    r = message.reply_to_message	
-    m = message
-    my = getattr(message, 'from_user', None)
-    ru = getattr(r, 'from_user', None)
-
-    if r:
-        reply_to_id = r.id
+        code = message.text.split(" ", 1)[1]
+    except:
+        return await message.reply('Command Incomplete!\nUsage: /eval your_python_code')
     old_stderr = sys.stderr
     old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
     try:
-        await aexec(
-		code=cmd, 
-		message=message,
-		my=my,
-		m=message, 
-		r=r,
-		ru=ru,
-		bot=bot
-	)
-    except Exception:
+        await aexec(code, client, message)
+    except:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
@@ -69,25 +33,21 @@ async def evaluate(bot, message):
     elif stdout:
         evaluation = stdout
     else:
-        evaluation = "Success"
-    taken_time = round((time.time() - start_time), 3)
-    output = evaluation.strip()
-    format_text = f"Output:\n{output}"
-    final_output = format_text
+        evaluation = "Success!"
+    final_output = f"Output:\n\n<code>{evaluation}</code>"
+    try:
+        await message.reply(final_output)
+    except MessageTooLong:
+        with open('eval.txt', 'w+') as outfile:
+            outfile.write(final_output)
+        await message.reply_document('eval.txt')
+        os.remove('eval.txt')
+
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
 	
-    if len(final_output) > 4096:
-        filename = "output.txt"
-        with open(filename, "w+", encoding="utf8") as out_file:
-            out_file.write(str(final_output))
-        await message.reply_document(
-            document=filename,
-            caption=f'`{cmd}`',
-            quote=True,
-            
-        )
-        os.remove(filename)
-        await status_message.delete()
-        return
-    else:
-        await status_message.edit(final_output, parse_mode=enums.ParseMode.HTML)
-        return
