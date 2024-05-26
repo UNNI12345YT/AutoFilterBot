6e752bdd8d37091bbac5447a9659a6a04464a4f6
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-26 20:09:29 +0530

    Update img.py

--- plugins/helpers/img.py
+++ plugins/helpers/img.py
@@ -1,25 +1,49 @@
-import asyncio
-from pyrogram import Client, filters
-import requests
-import wget 
+from pyrogram import Client, filters, types as t,errors
+import traceback,random,datetime,os,io
+import asyncio,base64,mimetypes,os
+from lexica import AsyncClient
+from lexica.constants import languageModels
+import httpx
+from urllib.parse import urlsplit
+from .pastebins import nekobin
+from bot import TelegraphClient
 
-@Client.on_message(filters.command(["image", "img", "image_serch", "photo"]))
-async def safoneapi(client, message):
+
+
+
+async def SearchImages(query,search_engine) -> dict:
+    client = AsyncClient()
+    output = await client.SearchImages(query,0,search_engine)
+    await client.close()
+    return output
+
+
+def getText(message):
+    """Extract Text From Commands"""
+    text_to_return = message.caption if message.caption else message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+def getImageContent(url):
+    """Get Image Content"""
     try:
-        user_query = ' '.join(message.command[1:])
-        if not user_query:
-            await message.reply_text("Please provide a keyword to search for an image.")
-        encoded_query = user_query.replace(" ", "%20")
-
-        response = requests.get(f"https://api.safone.dev/image?query={encoded_query}&limit=10")
-        if response.status_code == 200:
-            data = response.json()
-            image_data = data['results'][0]
-            image_url = image_data['imageUrl']
-            downloaded_image = wget.download(image_url)
-            kndi = await message.reply_text("`Serching...`")
-            await client.send_photo(message.chat.id, downloaded_image)     
-            kndi.delete()
-
-    except Exception as e:
-        await message.reply_text(f"An error occurred\nMybe it's error is api problem: {e}")
+        client = httpx.Client()
+        response = client.get(cleanUrl(url))
+        if response.status_code != 200:
+            return None
+        imageType = response.headers["content-type"].split("/")[1]
+        if imageType == "gif":
+            return None
+        return response.content
+    except (TimeoutError, httpx.ReadTimeout, httpx.ReadError):
+        return None
+        
+    
