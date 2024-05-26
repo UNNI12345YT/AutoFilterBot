7f639647a1ac1158fab662809f46c657eaa2f941
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-26 20:51:38 +0530

    Update img.py

--- plugins/helpers/img.py
+++ plugins/helpers/img.py
@@ -5,10 +5,34 @@ from lexica import AsyncClient
 from lexica.constants import languageModels
 import httpx
 from urllib.parse import urlsplit
-from .pastebins import nekobin
-from bot import TelegraphClient
+import httpx
 
 
+NEKOBIN = "https://nekobin.com/api/documents"
+async def nekobin(data,extension=None):
+    """
+    To Paste the given message/text/code to nekobin
+    """
+    try:
+        async with httpx.AsyncClient() as req:
+            res = req.post(
+                url=NEKOBIN,
+                json={
+                    "content":data,
+                    "title": "data",
+                    "author": "SDWaifuRobot"
+                })
+    except Exception as e:
+        return {"error": str(e)}
+    if res.ok:
+        resp = res.json()
+        purl = (
+            f"nekobin.com/{resp['result']['key']}.{extension}"
+            if extension
+            else f"nekobin.com/{resp['result']['key']}"
+        )
+        return purl
+    return {"error": "Unable to reach nekobin."}
 
 
 async def SearchImages(query,search_engine) -> dict:
