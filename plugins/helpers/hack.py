7029e88ae1f8e3e10aff9a190be5bdd179dd115b
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-24 17:50:28 +0530

    Update hack.py

--- plugins/helpers/hack.py
+++ plugins/helpers/hack.py
@@ -1,15 +1,15 @@
 import requests
 from pyrogram import Client, filters
 
-Client.on_message(filters.command("hack"))
-async def hackai(client, message):
+@Client.on_message(filters.command("hackai"))
+async def hack_ai(client, message):
+    query = message.text.split(' ', 1)[1]  
+    api = f"https://horrid-api.onrender.com/hackai?query={query}"
 
-
-mes = "what is your name"
-ow = "@UNNIdud"
-botname = "Natasha"
-api = f"https://horridluffyllamaapi-f66a74967a84.herokuapp.com/hackai?query={mes}"
-res = requests.get(api)
-dd = res.json()
-m = dd ['response']
-await message.reply_text(m)
+    try:
+        res = requests.get(api)
+        data = res.json()
+        response = data['response']
+        await message.reply_text(response)
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {str(e)}")
