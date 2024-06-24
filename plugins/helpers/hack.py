27e2274f7bffd8db7fa89878632abd5d8e902729
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-24 17:57:04 +0530

    Update hack.py

--- plugins/helpers/hack.py
+++ plugins/helpers/hack.py
@@ -3,13 +3,15 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("hack"))
 async def hack_ai(client, message):
-    query = message.text.split(' ', 1)[1]  
-    api = f"https://horrid-api.onrender.com/hackai?query={query}"
-
     try:
+        query = message.text.split(' ', 1)[1]
+        api = f"https://horrid-api.onrender.com/hackai?query={query}"
+
         res = requests.get(api)
         data = res.json()
         response = data['response']
         await message.reply_text(response)
+    except IndexError:
+        await message.reply_text("Please provide a query to proceed.")
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
