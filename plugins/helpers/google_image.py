a63ba264e653ce1c9b435f226f5a1dd3bed8c80d
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-30 19:47:16 +0530

    Update google_image.py

--- plugins/helpers/google_image.py
+++ plugins/helpers/google_image.py
@@ -1,38 +1,55 @@
 import requests
-from pyrogram import Client, filters
-from pyrogram.types import InputMediaPhoto
+import time
+import os
+from pyrogram import filters
+from REPO import app
+
+# Command handler for /generate
+@app.on_message(filters.command('img'))
+async def generate_image(client, message):
+    # Get the prompt from the command
+    prompt = ' '.join(message.command[1:])
+
+    # Send a message to inform the user to wait
+    wait_message = await message.reply_text("Please wait while I generate the image...")
+    StartTime = time.time()
 
 
-@Client.on_message(filters.command(["image", "img"]))
-async def pinterest(client, message):
-    chat_id = message.chat.id
+    # API endpoint URL
+    url = 'https://ai-api.magicstudio.com/api/ai-art-generator'
 
-    try:
-        query = message.text.split(maxsplit=1)[1]  # Get query string after command
-    except IndexError:
-        return await message.reply("Please provide an image name to search for.")
+    # Form data for the request
+    form_data = {
+        'prompt': prompt,
+        'output_format': 'bytes',
+        'request_timestamp': str(int(time.time())),
+        'user_is_subscribed': 'false',
+    }
 
-    url = f"https://api.safone.dev/image?q={query}"  # Construct the API URL
+    # Send a POST request to the API
+    response = requests.post(url, data=form_data)
 
-    # Make the request using requests library (remove 'get' from imports)
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for non-200 status codes
+    if response.status_code == 200:
+        try:
+            if response.content:
+                destination_dir = ''
+                destination_path = os.path.join(destination_dir, 'generated_image.jpg')
 
-    try:
-        images_data = response.json()
-        images = images_data["images"][:6]  # Limit to 6 images
-    except (KeyError, json.JSONDecodeError):
-        return await message.reply("Error fetching images from Pinterest.")
+                # Save the image to the destination path
+                with open(destination_path, 'wb') as f:
+                    f.write(response.content)
 
-    media_group = []
-    for image_url in images:
-        media_group.append(InputMediaPhoto(media=image_url))
+                # Delete the wait message
+                await wait_message.delete()
 
-    # Send media group instead of editing the message repeatedly
-    await message.reply("`Serching...`")
-    try:
-        await client.send_media_group(
-            chat_id=chat_id, media=media_group, reply_to_message_id=message.id
-        )
-    except Exception as e:
-        await message.reply(f"Error: {e}")
+                # Send the generated image
+                await message.reply_photo(destination_path, caption=f"Here's the generated image!\nTime Taken: {time.time() - StartTime}")
+
+                # Delete the generated image after sending
+                os.remove(destination_path)
+            else:
+                await wait_message.edit_text("Failed to generate the image.")
+        except Exception as e:
+            await wait_message.edit_text("Error: {}".format(e))
+    else:
+        await wait_message.edit_text("Error: {}".format(response.status_code))
