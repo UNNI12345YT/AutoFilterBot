206f73bf7c57ceb64a9c35874794e59c59b02619
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-07-02 20:12:17 +0530

    Update malayalam.py

--- plugins/helpers/malayalam.py
+++ plugins/helpers/malayalam.py
@@ -3,29 +3,29 @@ import requests
 
 @Client.on_message(filters.command('mgpt'))
 async def lexica_askbot(client, message):
-    text = message.text.split()[1:]
-    text = " ".join(text)
-    if not text:
-        await message.reply_text("Give An Input !!")
-    if len(text) == 0:
-        return await message.reply_text("Give An Input !!", parse_mode="Markdown")
-    txt = await message.reply_text("🔍")
-    payload = {
-        'messages': [
-            {
-                'role': "system",
-                'content': "your Are a malayalam talking assistant, Your owner is @UNNIdud",
-            },
-            {
-                'role': "user",
-                'content': text,
-            },
-        ],
-        "model_id": 19
-    }
+    text = message.text.split()[1:]
+    text = " ".join(text)
+    if not text:
+        await message.reply_text("Give An Input !!")
+    if len(text) == 0:
+        return await message.reply_text("Give An Input !!", parse_mode="Markdown")
+    txt = await message.reply_text("🔍")
+    payload = {
+        'messages': [
+            {
+                'role': "system",
+                'content': "your Are a malayalam talking assistant, Your owner is @UNNIdud",
+            },
+            {
+                'role': "user",
+                'content': text,
+            },
+        ],
+        "model_id": 19
+    }
 
-    api = 'https://api.qewertyy.dev/models'
+    api = 'https://api.qewertyy.dev/models'
 
-    response = requests.post(api, json=payload)
-    data = response.json()
-    await txt.edit(data['content'])
+    response = requests.post(api, json=payload)
+    data = response.json()
+    await txt.edit(data['content'])
