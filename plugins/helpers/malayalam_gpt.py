d33f461871e92445bd51bb335068eed151b92784
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-07-03 17:48:43 +0530

    Create malayalam_gpt.py

---
+++ plugins/helpers/malayalam_gpt.py
@@ -0,0 +1,37 @@
+from pyrogram import Client, filters
+import requests
+
+@Client.on_message(filters.command('mgpt'))
+async def lexica_askbot(client, message):
+    text = message.text.split()[1:]
+    text = " ".join(text)
+    
+    if not text:
+        await message.reply_text("Give An Input !!")
+        return
+    
+    txt = await message.reply_text("🔍")
+    
+    payload = {
+        'messages': [
+            {
+                'role': "system",
+                'content': "You are a Malayalam talking assistant. Your owner is @UNNIdud.",
+            },
+            {
+                'role': "user",
+                'content': text,
+            },
+        ],
+        "model_id": 38
+    }
+
+    api = 'https://api.qewertyy.dev/models'
+
+    try:
+        response = requests.post(api, json=payload)
+        response.raise_for_status()
+        data = response.json()
+        await txt.edit(data['content'])
+    except requests.exceptions.RequestException as e:
+        await txt.edit(f"Error: {str(e)}")
